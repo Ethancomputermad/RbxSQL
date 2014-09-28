3d3cc7844b22dcb3668a2e78102326bb8960137a
Ethancomputermad <ethanmayersweet@me.com>
2014-09-28 21:29:20 +0100

    Update RbxSQL.lua

--- RbxSQL.lua
+++ RbxSQL.lua
@@ -1,4 +1,4 @@
---Copyright(C) Ethan Sweet 2014, All Rights Reserved. Service may be terminated at any time. Permission to use the source below and service may be withdrawn at any time for any reason.
+--Copyright(C) Ethan Sweet / Ethancomputermad 2014, All Rights Reserved. Service may be terminated at any time. Permission to use the source below and service may be withdrawn at any time for any reason. This notice shall not be removed from the source, or any variation of the source, removing this notice shall result in immediete loss of permission to use the services and source.
 RSQL={}
 funcs={
 	['connect']=function(...) --host,username,password,database,port,socket
